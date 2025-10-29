460c9aab3a87f19f4e96642af4e4e123d4030f6f
b <a>
2025-10-29 12:14:31 +0100

    added calculation for boundaries

--- src/pde/system.h
+++ src/pde/system.h
@@ -2,41 +2,67 @@
 #define SYSTEM_H_
 #include "grid.h"
 #include "indexing.h"
+#include <array>
 #include <cstdint>
 
-struct Gridsize {
-    const double x;
-    const double y;
-    const double x_squared;
-    const double y_squared;
+struct Gridsize
+{
+  const double x;
+  const double y;
+  const double x_squared;
+  const double y_squared;
 
-    Gridsize(double x, double y)
-        : x(x), y(y), x_squared(x * x), y_squared(y * y) {}
+  Gridsize(double x, double y)
+    : x(x)
+    , y(y)
+    , x_squared(x * x)
+    , y_squared(y * y)
+  {
+  }
 };
 
-struct PDESystem {
-    const double Re;
-    double dt;
-    Grid2D p;
-    Grid2D u;
-    Grid2D v;
-    Grid2D F;
-    Grid2D G;
-    Grid2D b;
-    const uint16_t size_x;
-    const uint16_t size_y;
-    const Gridsize h;
+struct PDESystem
+{
+  const double Re;
+  double dt;
+  Grid2D p;
+  Grid2D u;
+  Grid2D v;
+  Grid2D F;
+  Grid2D G;
+  Grid2D b;
+  const uint16_t size_x;
+  const uint16_t size_y;
+  const Gridsize h;
+  const std::array<double, 2> boundaryBottom;
+  const std::array<double, 2> boundaryTop;
+  const std::array<double, 2> boundaryLeft;
+  const std::array<double, 2> boundaryRight;
 
-    PDESystem(double Re, double dt, uint16_t size_x, uint16_t size_y, double hx,
-              double hy)
-        : Re(Re), dt(dt), size_x(size_x), size_y(size_y),
-          p(Grid2D(size_x, size_y)), u(Grid2D(size_x, size_y)),
-          v(Grid2D(size_x, size_y)), F(Grid2D(size_x, size_y)),
-          G(Grid2D(size_x, size_y)), b(Grid2D(size_x, size_y)),
-          h(Gridsize(hx, hy)) {}
+  PDESystem(double Re, double dt, uint16_t size_x, uint16_t size_y, double hx,
+    double hy, std::array<double, 2> boundaryBottom,
+    std::array<double, 2> boundaryTop, std::array<double, 2> boundaryLeft,
+    std::array<double, 2> boundaryRight)
+    : Re(Re)
+    , dt(dt)
+    , size_x(size_x)
+    , size_y(size_y)
+    , p(Grid2D(size_x + 2, size_y + 2))
+    , u(Grid2D(size_x + 2, size_y + 2))
+    , v(Grid2D(size_x + 2, size_y + 2))
+    , F(Grid2D(size_x + 2, size_y + 2))
+    , G(Grid2D(size_x + 2, size_y + 2))
+    , b(Grid2D(size_x + 2, size_y + 2))
+    , h(Gridsize(hx, hy))
+    , boundaryBottom(boundaryBottom)
+    , boundaryTop(boundaryTop)
+    , boundaryLeft(boundaryLeft)
+    , boundaryRight(boundaryRight)
+  {
+  }
 };
 
 void timestep(PDESystem system);
-void print_pde_system(const PDESystem &sys);
+void print_pde_system(const PDESystem& sys);
 
 #endif // SYSTEM_H_
