fdf7cf24b14e57aaba68e2e5aa8b4a78d08765d5
Jonathan Ulmer <jonathan.ulmer@ulmail.de>
2025-11-14 09:34:47 +0100

    modify print

--- src/main.cc
+++ src/main.cc
@@ -9,16 +9,17 @@
 
 auto main(int argc, char* argv[]) -> int
 {
+  MPI_Init(&argc, &argv);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
   LOG::Init(LOG::LoggerType::STDOUT);
   if (argc < 2)
   {
     LOG::Warning("missing file name");
     return -1;
   }
-  MPI_Init(&argc, &argv);
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   if (!Settings::loadFromFile(argv[1]))
   {
@@ -29,8 +30,6 @@ auto main(int argc, char* argv[]) -> int
 
   PDESystem system = PDESystem(Settings::get());
 
-  print_pde_system(system);
-
   double time = 0;
   while (time < system.settings.endTime)
   {
@@ -39,6 +38,7 @@ auto main(int argc, char* argv[]) -> int
     std::cout << "\rTime: t=" << time << "\t dt=" << system.dt << std::flush;
     write_vtk(system, time);
   }
+  std::cout << std::endl;
 
   std::cout << "Hello from Rank " << rank << " of " << size << std::endl;
 
