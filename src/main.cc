635bf473047e403cd415dad9f79bf2f898994c99
Jonathan Ulmer <jonathan.ulmer@ulmail.de>
2025-12-01 17:55:40 +0100

    working solvers.
    not jet checked for correctness

--- src/main.cc
+++ src/main.cc
@@ -58,19 +58,19 @@ auto main(int argc, char* argv[]) -> int
     time += system.dt;
     step(system, time);
     time += system.dt;
-    for (int i = 0; i < mpiInfo.size; i++)
-    {
-      MPI_Barrier(MPI_COMM_WORLD);
-      if (mpiInfo.rank == i)
-      {
-        std::cout << "Hello from Rank " << rank << " of " << size << std::endl;
-        std::cout << "\rTime: t=" << time << "\t dt=" << system.dt << std::endl;
-        std::cout << "\rPressure: t=" << system.p << std::endl;
-        std::cout << "\r V: " << system.v << "\t U:" << system.u << std::endl;
-      }
-    }
-    break;
-    //   write_vtk(system, time);
+    // for (int i = 0; i < mpiInfo.size; i++)
+    //{
+    //   MPI_Barrier(MPI_COMM_WORLD);
+    //   if (mpiInfo.rank == i)
+    //   {
+    //     std::cout << "Hello from Rank " << rank << " of " << size << std::endl;
+    //     std::cout << "\rTime: t=" << time << "\t dt=" << system.dt << std::endl;
+    //     std::cout << "\rPressure: t=" << system.p << std::endl;
+    //     std::cout << "\r V: " << system.v << "\t U:" << system.u << std::endl;
+    //   }
+    // }
+    //  break;
+    //     write_vtk(system, time);
   }
   std::cout << std::endl;
   ProfilePop();
--- src/pde/pressuresolvers.cpp
+++ src/pde/pressuresolvers.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdint>
+#include <cstdlib>
 #include <grid/grid.h>
 #include <grid/indexing.h>
 #include <ios>
@@ -25,15 +26,20 @@ void solve(CGSolver& cg, PDESystem& system)
 
   // cg.residual = system.rhs - A*system.p;
   //  broadcast_boundary(copy_with_offset, system.p.boundary, system.p);
+  // MPI_COMM_BUFFER* comm_p = new MPI_COMM_BUFFER(system.p, system.p.boundary.all, MPI_COMM_WORLD, system.partitioning);
+  // delete comm_p;
+
   broadcast_boundary(copy_with_offset, system.partitioning, system.p.boundary, system.p);
-  broadcast_halo(copy, system.p.boundary, system.p, cg.search_direction);
-  // cg.residual[I] = s.rhs[I] - A(s.p, I);
-  parallel_broadcast(aAxpy, system.p.range, cg.residual, -1., A, system.p, system.rhs);
+  // broadcast_boundary(copy, system.partitioning, system.p.boundary, system.p, cg.search_direction);
+  //
+  //  cg.residual[I] = s.rhs[I] - A(s.p, I);
+  broadcast(aAxpy, system.p.range, cg.residual, -1., A, system.p, system.rhs);
 
   residual_norm = dot(cg.residual, cg.residual);
 
+  // ensure correct ghosts
   // cg.search_direction = cg.residual;
-  parallel_broadcast(copy, system.p.range, Offset { 0, 0 }, cg.residual, cg.search_direction);
+  distributed_broadcast(copy, system.partitioning, system.p.range, cg.search_direction, Offset { 0, 0 }, cg.residual, cg.search_direction);
 
   for (int iter = 0; iter < Settings::get().maximumNumberOfIterations; iter++)
   {
@@ -41,19 +47,41 @@ void solve(CGSolver& cg, PDESystem& system)
     old_residual_norm = residual_norm;
 
     broadcast_boundary(copy_with_offset, system.partitioning, cg.search_direction.boundary, cg.search_direction);
-    // broadcast_boundary(copy, system.p.boundary, system.p, cg.search_direction);
+
     double alpha = residual_norm / Adot(A, cg.search_direction, cg.search_direction);
     // DebugF("Alpha: {}", alpha);
 
     // system.p = system.p + a * cg.search_direction;
-    broadcast(axpy, system.p.range, system.p, alpha, cg.search_direction, system.p);
-    broadcast(axpy, system.p.boundary.all, system.p, alpha, cg.search_direction, system.p);
+    broadcast(axpy, { system.p.begin, system.p.end }, system.p, alpha, cg.search_direction, system.p);
+    // broadcast_ghosts(axpy, system.partitioning, system.p.boundary, system.p, alpha, cg.search_direction, system.p);
+    //  broadcast(axpy, system.p.boundary.all, system.p, alpha, cg.search_direction, system.p);
 
     // cg.residual = cg.residual - a * A * cg.search_direction;
     broadcast(aAxpy, system.p.range, cg.residual, -alpha, A, cg.search_direction, cg.residual);
 
-    std::cout << "\rResidual:\t" << residual_norm << " Iterations:\t" << iter << std::endl;
-    if (cg.residual.max() < Settings::get().epsilon)
+    double residual = cg.residual.max();
+    if (residual > 1e5)
+    {
+      ErrorF("residual exploded {}", residual);
+
+      for (int i = 0; i < system.partitioning.size; i++)
+      {
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (system.partitioning.rank == i)
+        {
+          std::cout << "Hello from Rank " << system.partitioning.rank << " of " << system.partitioning.size << std::endl;
+          DebugF("Alpha: {}", alpha);
+          std::cout << "CG Solver res" << cg.residual << std::endl;
+          std::cout << "CG Solver p" << cg.search_direction << std::endl;
+          std::cout << "Pressure: " << system.p << std::endl;
+          std::cout << "RHS: " << system.rhs << std::endl;
+        }
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
+      abort();
+    }
+
+    if (residual < Settings::get().epsilon)
     {
       DebugF("Finished Pressure solver in inter {}", iter);
       // update Pressure ghosts
@@ -70,7 +98,7 @@ void solve(CGSolver& cg, PDESystem& system)
     // TODO Update Ghosts
     // cg.search_direction[I] = cg.residual[I] + beta * cg.search_direction[I];
     distributed_broadcast(axpy, system.partitioning, system.p.range, cg.search_direction, cg.search_direction, beta, cg.search_direction, cg.residual);
-    // broadcast(axpy, system.p.range, cg.search_direction, beta, cg.search_direction, cg.residual);
+    MPI_Barrier(MPI_COMM_WORLD);
   }
   MPI_Barrier(MPI_COMM_WORLD);
   broadcast_boundary(copy_with_offset, system.partitioning, system.p.boundary, system.p);
@@ -100,11 +128,39 @@ void solve(SORSolver& S, PDESystem& system)
     system.residual = 0;
     broadcast_boundary(copy_with_offset, system.partitioning, system.p.boundary, system.p);
     broadcast_black(sor_step, system.p.range, system);
-    if (iter % 10 && system.residual < Settings::get().epsilon)
+    MPI_COMM_BUFFER* comm_black = new MPI_COMM_BUFFER(system.p, system.p.boundary.all, MPI_COMM_WORLD, system.partitioning);
+    delete comm_black;
+    broadcast_red(sor_step, system.p.range, system);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_COMM_BUFFER* comm_red = new MPI_COMM_BUFFER(system.p, system.p.boundary.all, MPI_COMM_WORLD, system.partitioning);
+    delete comm_red;
+
+    double local_residual = system.residual;
+    double global_residual = 0.;
+    MPI_Allreduce(&local_residual, &global_residual, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    if (global_residual > 1e16)
+    {
+      ErrorF("residual exploded {}", global_residual);
+
+      for (int i = 0; i < system.partitioning.size; i++)
+      {
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (system.partitioning.rank == i)
+        {
+          std::cout << "Hello from Rank " << system.partitioning.rank << " of " << system.partitioning.size << std::endl;
+          std::cout << "Pressure: " << system.p << std::endl;
+          std::cout << "RHS: " << system.rhs << std::endl;
+        }
+      }
+      abort();
+    }
+
+    if (iter % 10 && global_residual < Settings::get().epsilon)
     {
 
       // std::cout << std::scientific << std::setprecision(14) << "Residual: " << system.residual << std::endl;
-      // std::cout << "\nSOR converged after n=" << iter << " Iterations" << std::endl;
+      //  std::cout << "\nSOR converged after n=" << iter << " Iterations" << std::endl;
       break;
     }
   }
